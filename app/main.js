e9268b67866f01cc22fcf80f4a1a14ed850e9dfc
kvnlnt <kvnlnt@yahoo.com>
2018-07-18 23:10:35 -0700

    refactors

--- app/main.js
+++ app/main.js
@@ -5,45 +5,54 @@ const template = require("../templates/index");
 
 // setup
 console.time("Styler");
-if (!fs.existsSync("./docs")) fs.mkdirSync("./docs");
+if (!fs.existsSync("./public")) fs.mkdirSync("./public");
 
 // theme files
-const importThemeFile = (t, f) => {
-  let o = require(`../themes/base/${f}`);
-  const isCustom = t !== "base" && fs.existsSync(`./themes/${t}/${f}`);
-  if (isCustom) o = Object.assign(o, require(`../themes/${t}/${f}`));
+const importStyleConfig = (t, f) => {
+  let o = require(`../styles/base/${f}`);
+  const isCustom = t !== "base" && fs.existsSync(`./styles/${t}/${f}`);
+  if (isCustom) o = Object.assign(o, require(`../styles/${t}/${f}`));
   return o;
 };
 
-const config = importThemeFile(theme, "config.json");
-const params = importThemeFile(theme, "params.json");
-const atoms = importThemeFile(theme, "atoms.json");
-const molecules = importThemeFile(theme, "molecules.json");
-const patterns = importThemeFile(theme, "patterns.json");
-const resets = importThemeFile(theme, "resets.json");
+const config = importStyleConfig(theme, "config.json");
+const params = importStyleConfig(theme, "params.json");
+const atoms = importStyleConfig(theme, "atoms.json");
+const molecules = importStyleConfig(theme, "molecules.json");
+const patterns = importStyleConfig(theme, "patterns.json");
+const resets = importStyleConfig(theme, "resets.json");
 
 // output
-const outPath = `./docs/`;
+const outPath = `./public/`;
 const outFile = `${config.name}.${config.version}.css`;
-const outGuide = `${config.name}.${config.version}.html`;
 
 // precompile
-const precompiledAtoms = compiler.precompileAtoms(atoms, compiler.interpolate, params);
-const precompiledMolecules = compiler.precompileMolecules(molecules, precompiledAtoms);
+const precompiledAtoms = compiler.precompileAtoms(
+  atoms,
+  compiler.interpolate,
+  params
+);
+const precompiledMolecules = compiler.precompileMolecules(
+  molecules,
+  precompiledAtoms
+);
 const precompiledResets = compiler.precompileResets(resets);
 const precompiledPatterns = compiler.precompilePatterns(patterns);
 
 // compile
 const compiledAtoms = compiler.compileRules(precompiledAtoms).join("");
 const compiledMolecules = compiler.compileRules(precompiledMolecules).join("");
-const compiledPatterns = template(outFile, precompiledPatterns.links.join(""), precompiledPatterns.markup.join(""));
+const compiledPatterns = template(
+  outPath + outFile,
+  precompiledPatterns.links.join(""),
+  precompiledPatterns.markup.join("")
+);
 const compiledResets = compiler.compileRules(precompiledResets).join("");
 
 // render
 const styles = `${compiledResets}${compiledMolecules}${compiledAtoms}`;
 fs.writeFileSync(outPath + outFile, styles);
-fs.writeFileSync(outPath + outGuide, compiledPatterns);
-if (theme === "base") fs.writeFileSync(outPath + "index.html", compiledPatterns);
+if (theme === "base") fs.writeFileSync("index.html", compiledPatterns);
 
 // Running time
 console.timeEnd("Styler");
