a6a7b5a8bb2be8695ac315402a491b4d08cd38fb
kvnlnt <kvnlnt@yahoo.com>
2018-07-26 22:45:45 -0700

    wip: responsifier

--- tasks/guide.js
+++ tasks/guide.js
@@ -2,10 +2,30 @@ const fs = require("fs");
 let index = fs.readFileSync("./src/patterns/index.tmpl", "utf-8");
 
 module.exports = stylesheet => {
-  index = index.replace('<!-- STYLESHEET -->', stylesheet);
-  index = index.replace('<!-- BUTTONS -->', fs.readFileSync("./src/patterns/buttons.html", "utf-8"));
-  index = index.replace('<!-- MENU -->', fs.readFileSync("./src/patterns/menu.html", "utf-8"));
-  index = index.replace('<!-- COLORS -->', fs.readFileSync("./src/patterns/colors.html", "utf-8"));
-  index = index.replace('<!-- TYPOGRAPHY -->', fs.readFileSync("./src/patterns/typography.html", "utf-8"));
+  index = index.replace("<!-- STYLESHEET -->", stylesheet);
+  index = index.replace(
+    "<!-- BUTTONS -->",
+    fs.readFileSync("./src/patterns/buttons.html", "utf-8")
+  );
+  index = index.replace(
+    "<!-- COLORS -->",
+    fs.readFileSync("./src/patterns/colors.html", "utf-8")
+  );
+  index = index.replace(
+    "<!-- GRIDS -->",
+    fs.readFileSync("./src/patterns/grids.html", "utf-8")
+  );
+  index = index.replace(
+    "<!-- MENU -->",
+    fs.readFileSync("./src/patterns/menu.html", "utf-8")
+  );
+  index = index.replace(
+    "<!-- RESPONSIFIER -->",
+    fs.readFileSync("./src/patterns/responsifier.html", "utf-8")
+  );
+  index = index.replace(
+    "<!-- TYPOGRAPHY -->",
+    fs.readFileSync("./src/patterns/typography.html", "utf-8")
+  );
   return index;
 };
