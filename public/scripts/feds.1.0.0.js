36d2544152a078731099e79957117d864120344a
kvnlnt <kvnlnt@yahoo.com>
2018-07-31 17:36:42 -0700

    wip: responsifier

--- public/scripts/feds.1.0.0.js
+++ public/scripts/feds.1.0.0.js
@@ -1,83 +1,90 @@
-var feds = (function (m) {
+var feds = (function(m) {
   function ContainerQuery(opts) {
     var opts = opts || {};
     this.target = opts.target;
     this.queries = opts.queries;
-    this.classList = []; // [[className,eventLastAddedBy,eventLastRemovedBy]]
     this.locked = [];
     this.registerListeners();
   }
 
   ContainerQuery.prototype = {
-    classesAdd: function (query) {
+    classesAdd: function(query) {
       var that = this;
       if (!query.add) return this;
-      query.add.forEach(function (c) {
+      query.add.forEach(function(c) {
+        console.log("add", c);
         if (!that.classIsLocked(c)) that.target.classList.add(c);
       });
       return this;
     },
-    classIsLocked: function (c) {
-      console.log(this.locked);
+    classIsLocked: function(c) {
       return this.locked.indexOf(c) > -1;
     },
-    classesLock: function (query) {
+    classesLock: function(query) {
       var that = this;
       if (!query.lock) return this;
-      query.lock.forEach(function (c) {
+      query.lock.forEach(function(c) {
+        console.log("lock", c);
         if (!that.classIsLocked(c)) that.locked.push(c);
       });
       return this;
     },
-    classesRemove: function (query) {
+    classesRemove: function(query) {
       var that = this;
       if (!query.remove) return this;
-      query.remove.forEach(function (c) {
+      query.remove.forEach(function(c) {
+        console.log("remove", that.classIsLocked(c));
         if (!that.classIsLocked(c)) that.target.classList.remove(c);
       });
       return this;
     },
-    classesToggle: function (query) {
+    classesToggle: function(query) {
       var that = this;
       if (!query.toggle) return this;
-      query.toggle.forEach(function (c) {
+      query.toggle.forEach(function(c) {
+        console.log("toggle", c);
         if (!that.classIsLocked(c)) that.target.classList.toggle(c);
       });
       return this;
     },
-    classesUnlock: function (query) {
+    classesUnlock: function(query) {
       var that = this;
       if (!query.unlock) return this;
-      query.unlock.forEach(function (c) {
+      query.unlock.forEach(function(c) {
+        console.log("unlock", c);
         that.locked.splice(that.locked.indexOf(c), 1);
       });
       return this;
     },
-    handleClick: function (query) {
+    handleClick: function(query) {
       this.process(query);
-    },
-    handleResize: function (query) {
-      var r0 = parseInt(query.range[0]);
-      var r1 = parseInt(query.range[1] === "*" ? 100000 : query.range[1]);
-      if (query.source.innerWidth >= r0 && query.source.innerWidth <= r1) this.process(query);
       return this;
     },
-    handleScroll: function (query) {
+    handleResize: function(query) {
       var r0 = parseInt(query.range[0]);
       var r1 = parseInt(query.range[1] === "*" ? 100000 : query.range[1]);
-      if (query.source.scrollY >= r0 && query.source.scrollY <= r1) this.process(query);
+      if (query.source.innerWidth >= r0 && query.source.innerWidth <= r1)
+        this.process(query);
       return this;
     },
-    process: function (query) {
-      if (query.lock) this.classesLock(query);
+    handleScroll: function(query) {
+      var r0 = parseInt(query.range[0]);
+      var r1 = parseInt(query.range[1] === "*" ? 100000 : query.range[1]);
+      if (query.source.scrollY >= r0 && query.source.scrollY <= r1)
+        this.process(query);
+      return this;
+    },
+    process: function(query) {
       if (query.unlock) this.classesUnlock(query);
       if (query.add) this.classesAdd(query);
       if (query.remove) this.classesRemove(query);
       if (query.toggle) this.classesToggle(query);
+      if (query.lock) this.classesLock(query);
+      return this;
     },
-    registerListeners: function () {
+    registerListeners: function() {
       var that = this;
-      this.queries.forEach(function (i) {
+      this.queries.forEach(function(i) {
         var scroll = that.handleScroll.bind(that, i);
         var resize = that.handleResize.bind(that, i);
         var click = that.handleClick.bind(that, i);
--- public/scripts/guide.js
+++ public/scripts/guide.js
@@ -1,17 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
   new feds.ContainerQuery({
     target: document.querySelector("#top"),
-    queries: [{
+    queries: [
+      {
         source: window,
         event: "scroll",
         range: [0, 199],
         add: ["hide"]
-      }, {
+      },
+      {
         source: window,
         event: "scroll",
         range: [200, "*"],
         remove: ["hide"]
-      }, {
+      },
+      {
         source: window,
         event: "resize",
         range: [0, 799],
@@ -24,11 +27,6 @@ document.addEventListener("DOMContentLoaded", function () {
         range: [800, "*"],
         remove: ["hide"],
         unlock: ["hide"]
-      },
-      {
-        source: document.querySelector("#top"),
-        event: "click",
-        toggle: ["hide"]
       }
     ]
   });
