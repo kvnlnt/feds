7a14865b2d0c5d2ca644c63e4048db6fb23f11eb
kvnlnt <kvnlnt@yahoo.com>
2018-07-31 21:26:22 -0700

    wip: responsifier

--- public/scripts/guide.js
+++ public/scripts/guide.js
@@ -1,4 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
+  // grid
+  new feds.ContainerQuery({
+    target: document.querySelector("#responsiveGrid"),
+    queries: [
+      {
+        source: window,
+        event: "resize",
+        range: [0, 799],
+        add: ["flex-dir-col"]
+      },
+      {
+        source: window,
+        event: "resize",
+        range: [800, "*"],
+        remove: ["flex-dir-col"]
+      }
+    ]
+  });
+
+  // top link
   new feds.ContainerQuery({
     target: document.querySelector("#top"),
     queries: [
