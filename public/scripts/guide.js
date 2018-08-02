f43fc7da7e841164c360bca8700a313e22973f46
Kevin Lint <kevin.lint@emergencyreporting.com>
2018-08-02 13:16:39 -0700

    Adds responsifier demo

--- public/scripts/guide.js
+++ public/scripts/guide.js
@@ -1,28 +1,45 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // grid
+document.addEventListener("DOMContentLoaded", function () {
+
+  // LAYOUT
   new feds.ContainerQuery({
-    target: document.querySelector("#responsiveGrid"),
-    queries: [
-      {
+    target: document.querySelector("#layout"),
+    queries: [{
         source: window,
         event: "resize",
-        range: [0, 799],
+        range: [0, 999],
         add: ["flex-dir-col"]
       },
       {
         source: window,
         event: "resize",
-        range: [800, "*"],
+        range: [1000, "*"],
         remove: ["flex-dir-col"]
       }
     ]
   });
 
-  // top link
+  // GRID
+  new feds.ContainerQuery({
+    target: document.querySelector("#responsiveGrid"),
+    queries: [{
+        source: window,
+        event: "resize",
+        range: [0, 999],
+        add: ["flex-dir-col"]
+      },
+      {
+        source: window,
+        event: "resize",
+        range: [1000, "*"],
+        remove: ["flex-dir-col"]
+      }
+    ]
+  });
+
+  // TOP
   new feds.ContainerQuery({
     target: document.querySelector("#top"),
-    queries: [
-      {
+    queries: [{
         source: window,
         event: "scroll",
         range: [0, 199],
@@ -37,17 +54,76 @@ document.addEventListener("DOMContentLoaded", function() {
       {
         source: window,
         event: "resize",
-        range: [0, 799],
+        range: [0, 999],
         add: ["hide"],
         lock: ["hide"]
       },
       {
         source: window,
         event: "resize",
-        range: [800, "*"],
+        range: [1000, "*"],
         remove: ["hide"],
         unlock: ["hide"]
       }
     ]
   });
+
+  // RESPONSIFIER
+  new feds.ContainerQuery({
+    target: document.querySelector("#responsiveUIBody"),
+    queries: [{
+        source: window,
+        event: "resize",
+        range: [0, 799],
+        add: ["flex-dir-col"]
+      },
+      {
+        source: window,
+        event: "resize",
+        range: [800, "*"],
+        remove: ["flex-dir-col"]
+      }
+    ]
+  });
+
+  new feds.ContainerQuery({
+    target: document.querySelector("#responsiveUIMainMenu"),
+    queries: [{
+        source: window,
+        event: "resize",
+        range: [0, 799],
+        add: ["hide"]
+      },
+      {
+        source: window,
+        event: "resize",
+        range: [800, "*"],
+        remove: ["hide"]
+      },
+      {
+        source: document.querySelector("#responsiveUIToggleMenu"),
+        event: "click",
+        toggle: ["hide"]
+      }
+    ]
+  });
+
+  new feds.ContainerQuery({
+    target: document.querySelector("#responsiveUIToggleMenu"),
+    queries: [{
+        source: window,
+        event: "resize",
+        range: [0, 799],
+        remove: ["hide"]
+      },
+      {
+        source: window,
+        event: "resize",
+        range: [800, "*"],
+        add: ["hide"]
+      }
+    ]
+  });
+
+
 });
