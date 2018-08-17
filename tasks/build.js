0934ecda0a4842c337e266d6f4b7d2202cdcc249
kvnlnt <kvnlnt@yahoo.com>
2018-08-16 22:10:44 -0700

    factors our themes, adds in tests

--- tasks/build.js
+++ tasks/build.js
@@ -1,2 +1,10 @@
 require("./styles");
 require("./scripts");
+
+// increment build
+const color = require("./lib/color");
+const fs = require("fs");
+const pkg = require("../package.json");
+pkg.build += 1;
+fs.writeFileSync("./package.json", JSON.stringify(pkg, null, 2), "utf-8");
+console.log(color.ok("Build: " + pkg.build));
