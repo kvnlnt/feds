399fd94bd7b1479c33855a763ad393a17d5d630f
kvnlnt <kvnlnt@yahoo.com>
2018-11-28 21:21:48 -0800

    wip: navigations

--- public/scripts/feds.1.0.0.js
+++ public/scripts/feds.1.0.0.js
@@ -1,4 +1,4 @@
-// feds.1.0.0.505
+// feds.1.0.0.536
 (function () {
   'use strict';
 
@@ -71,7 +71,7 @@
     return _assertThisInitialized(self);
   }
 
-  var version = "1.0.0.505";
+  var version = "1.0.0.536";
 
   var Component = function Component() {
     var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "";
--- src/scripts/version.js
+++ src/scripts/version.js
@@ -1,1 +1,1 @@
-export default "1.0.0.505";
+export default "1.0.0.536";
