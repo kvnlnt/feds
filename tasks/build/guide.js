1565908fd26a56fd0925d87eff384feec3b1dd8d
Kevin Lint <kevin.lint@emergencyreporting.com>
2018-11-30 14:04:00 -0800

    autogen atoms in atom header

--- tasks/build/guide.js
+++ tasks/build/guide.js
@@ -9,6 +9,7 @@ const hb = require("handlebars");
 const wfs = fse.outputFileSync;
 const cfs = fse.copyFileSync;
 const rfs = fs.readFileSync;
+const stylus = require("stylus");
 
 // handlebars helpers
 hb.registerHelper("page", function(options) {
@@ -19,9 +20,13 @@ hb.registerHelper("page", function(options) {
     href="/guide/${options.hash.section.toLowerCase()}" 
     class="button text-size-s font-style-uppercase bg-color-grey-l border-radius-none border-size-none leading-s">
     ${options.hash.section}</a>`;
-  page += `<h2 class="guide ${options.hash.section ? "margin-top-2xl" : ""}">${options.hash.title}</h2>`;
+  page += `<h2 class="guide ${options.hash.section ? "margin-top-2xl" : ""}">${
+    options.hash.title
+  }</h2>`;
   if (options.hash.source)
-    page += `<a href="${guide.repo}${options.hash.source}" target="blank">${options.hash.source}</a>`;
+    page += `<a href="${guide.repo}${options.hash.source}" target="blank">${
+      options.hash.source
+    }</a>`;
   page += `${options.fn(this)}</section>`;
   return page;
 });
@@ -29,24 +34,82 @@ hb.registerHelper("page", function(options) {
 hb.registerPartial(
   "header",
   `{{#if title}}<h3 class="margin-top-xl margin-bot-none" id="{{title}}">{{title}}</h3>{{/if}}
-  <p class="text-size-xs opacity-70 margin-top-none margin-bot-xs">
-  {{#if source}}<a href="{{guide.repo}}{{source}}" target="blank">{{source}}</a></p>{{/if}}
-  <p class="margin-top-xs margin-bot-l">{{{descr}}}</p>`
+  {{#if source}}<p class="text-size-xs opacity-70 margin-top-none margin-bot-xs"><a href="{{guide.repo}}{{source}}" target="blank">{{source}}</a></p>{{/if}}
+  {{#if descr}}<p class="margin-top-xs margin-bot-l">{{{descr}}}</p>{{/if}}`
 );
+
+function renderAtoms(source, cb) {
+  const dir = __dirname + "../../../";
+  let src = "";
+  src += rfs(dir + "./src/styles/functions.styl", "utf-8");
+  src += rfs(dir + "./src/styles/variables.styl", "utf-8");
+  src += rfs(dir + source, "utf-8");
+  const cssSelectorReg = /([^\r\n,{}]+)(,(?=[^}]*{)|\s*{)/gm;
+  let atoms = "<p>";
+  stylus.render(src, function(err, css) {
+    if (err) throw err;
+    var i;
+    while ((i = cssSelectorReg.exec(css)) !== null) {
+      const selector = i[1].trim();
+      const isMediaQry = selector.indexOf("@media") === 0;
+      const isMobilePrefix = selector.indexOf(".mobile") === 0;
+      const isTabletPrefix = selector.indexOf(".tablet") === 0;
+      const isDesktopPrefix = selector.indexOf(".desktop") === 0;
+      const isMediaPrefix = isMobilePrefix || isTabletPrefix || isDesktopPrefix;
+      const isHoverPrefix = selector.indexOf(".hover") === 0;
+      const isActivePrefix = selector.indexOf(".active") === 0;
+      const isFocusPrefix = selector.indexOf(".focus") === 0;
+      const isTargetPrefix = selector.indexOf(".target") === 0;
+      const isCheckedPrefix = selector.indexOf(".checked") === 0;
+      const isStatePrefix =
+        isHoverPrefix || isFocusPrefix || isTargetPrefix || isCheckedPrefix;
+      let bgColor = "bg-color-primary-xl";
+      if (isMediaPrefix) bgColor = "bg-color-light-grey-l";
+      if (isStatePrefix) bgColor = "bg-color-light-grey";
+      const cleanSelector = selector.replace("\\", "");
+      const isValid = !isMediaQry;
+      if (isValid) {
+        atoms += `<span class="inline-block pad-left-s pad-right-s leading-l margin-right-xs margin-bot-xs font-size-xs ${bgColor} border-radius-m">${cleanSelector}</span>`;
+      }
+    }
+  });
+  atoms += "</p>";
+  return atoms;
+}
+
+hb.registerHelper("atom-header", function(options) {
+  const title = options.hash.title
+    ? `<h3 class="margin-top-xl margin-bot-none" id="${options.hash.title}">${
+        options.hash.title
+      }</h3>`
+    : "";
+  const source = options.hash.source
+    ? `<p class="text-size-xs opacity-70 margin-top-none margin-bot-xs"><a href="${
+        guide.repo
+      }${options.hash.source}" target="blank">${options.hash.source}</a></p>`
+    : "";
+  const descr = options.hash.descr
+    ? `<p class="margin-top-xs margin-bot-l">${options.hash.descr}</p>`
+    : "";
+  const atoms =
+    options.hash.atoms === false ? "" : renderAtoms(options.hash.source);
+  return `${title}${source}${descr}${atoms}`;
+});
 
 hb.registerHelper("example", function(options) {
   const flex = options.hash.flex === false ? false : true;
-  const title = options.hash.title ? `<h4 class="margin-bot-none">${options.hash.title}</h4>` : "";
-  const descr = options.hash.descr ? `<p class="margin-top-xs">${options.hash.descr}</p>` : "";
+  const title = options.hash.title
+    ? `<h4 class="margin-bot-none">${options.hash.title}</h4>`
+    : "";
+  const descr = options.hash.descr
+    ? `<p class="margin-top-xs">${options.hash.descr}</p>`
+    : "";
   const example = `<div class="shadow pad-2xl bg-color-white pos-relative">
   <div class="${flex ? "flex flex-wrap" : ""}">${options.fn(this)}</div></div>`;
-  const code = options.hash.code ? `<pre><code data-language="html">${options.fn(this)}</code></pre>` : "";
-  return `
-  ${title}
-  ${descr}
-  ${example}
-  ${code}
-  `;
+  const code = options.hash.code
+    ? `<pre><code data-language="html">${options.fn(this)}</code></pre>`
+    : "";
+  return `${title}${descr}${example}${code}`;
 });
 
 hb.registerHelper("code", function(options) {
--- tests/smoke.js
+++ tests/smoke.js
@@ -10,5 +10,5 @@ test("My first test", async t => {
 
     // Use the assertion to check if the actual header text is equal to the expected one
     .expect(Selector("#article-header").innerText)
-    .eql("Thank you, John Smith!");
+    .eql("Thank you, Committee");
 });
